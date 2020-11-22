4fd645c7ef87f33fdf9bbeeb2d2677962fd7c57a
Jesús Enrique Rascón <blograso@gmail.com>
2020-11-22 13:56:04 +0100

    NOW WE TWEET

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import SpotifyWebApi from "spotify-web-api-node"
 const server = express();
 const dot_env = dotenv.config()
 
+
+// AUTHENTICATION OF APPS
 var client = new Twitter({
   consumer_key: process.env.API_KEY,
   consumer_secret: process.env.API_SECRET_KEY,
@@ -16,25 +18,43 @@ var client = new Twitter({
 var spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_ID,
     clientSecret: process.env.SPOTIFY_SECRET
-  });
+});
+
 spotifyApi.setAccessToken(process.env.SPOTIFY_TOKEN);
 
+
+// ACTUAL APP
+
+// var title = null
+// var artist = null
+// var duration = null
+// var link = null
 
 spotifyApi.getMyCurrentPlaybackState()
   .then(function(data) {
     // Output items
     if (data.body && data.body.is_playing) {
+      
       console.log(data.body.item.name);
       console.log(data.body.item.artists[0].name);
+      console.log(data.body.item.external_urls.spotify);
+      console.log(data.body);
+      
+      var title = data.body.item.name;
+      var artist = data.body.item.artists[0].name;
+      var link = data.body.item.external_urls.spotify;
+
+      var tw = "Jesi is currently playing " + title + " by " + artist + "\n\n" + link
+      client.post('statuses/update', {status: tw}, function(error, tweet, response) {
+        if (!error) {
+            console.log(tweet);
+        }
+      });
+
     } else {
       console.log("User is not playing anything, or doing so in private.");
     }
   }, function(err) {
     console.log('Something went wrong!', err);
-  });
-// var params = {screen_name: 'elBotdeJesi'};
-// client.post('statuses/update', {status: 'I am a tweeting from js'}, function(error, tweet, response) {
-//     if (!error) {
-//       console.log(tweet);
-//     }
-//   });
+});
+
